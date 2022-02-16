a3b7f0ca311becdb722a732633faf6befe1b13ea
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-02-15 23:12:41 -0600

    User class

---
+++ src/main/java/com/example/springbootloginregistration/user/User.java
@@ -0,0 +1,77 @@
+package com.example.springbootloginregistration.user;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+public class User implements UserDetails {
+
+  @Id
+  @SequenceGenerator(
+    name = "user_sequence",
+    sequenceName = "user_sequence",
+    allocationSize = 1
+  )
+  @GeneratedValue(
+    strategy = GenerationType.SEQUENCE,
+    generator = "user_sequence"
+  )
+  private Long id;
+  private String name;
+  private String username;
+  private String email;
+  private String password;
+  @Enumerated(EnumType.STRING)
+  private UserRole appUserRole;
+  private Boolean locked;
+  private Boolean enabled;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+      SimpleGrantedAuthority authority =
+              new SimpleGrantedAuthority(appUserRole.name());
+      return Collections.singletonList(authority);
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+
+}
