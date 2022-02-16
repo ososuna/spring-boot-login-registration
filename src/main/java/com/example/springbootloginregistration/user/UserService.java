02dea322447da7b94885a3730d23428b17d6e869
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-02-15 23:22:05 -0600

    User service

---
+++ src/main/java/com/example/springbootloginregistration/user/UserService.java
@@ -0,0 +1,24 @@
+package com.example.springbootloginregistration.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+  private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return userRepository.findByEmail(email)
+      .orElseThrow(() ->
+        new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+  }
+
+}
