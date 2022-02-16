97bd81521534c600434365ee6c3b55e25230f696
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-02-15 23:35:54 -0600

    Registration controller & service

---
+++ src/main/java/com/example/springbootloginregistration/registration/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.example.springbootloginregistration.registration;
+
+public class RegistrationService {
+  
+  public String register(RegistrationRequest request) {
+    return "works";
+  }
+
+}
